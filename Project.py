6364568c17020d1989148868c2b2f863c4839556
anshmishra4444 <118624420+anshmishra4444@users.noreply.github.com>
2022-11-19 22:23:40 +0530

    Add files via upload
    Email Slicing in Python

---
+++ Project.py
@@ -0,0 +1,17 @@
+
+
+
+email_id = input("Enter email id: ")
+
+username = email_id[:email_id.index('@')]
+
+domain = email_id[email_id.index('@')+1:]
+
+a=domain.upper()
+print(f"username: {username} and domain: {(a)}")
+
+
+
+
+
+
