ab45a1f6dbe9dfc4ed5ef49c8bd75fabab90fcbb
anshmishra4444 <118624420+anshmishra4444@users.noreply.github.com>
2022-12-03 11:16:34 +0530

    Update Project.py

--- Project.py
+++ Project.py
@@ -1,14 +1,14 @@
 
 
+while(True):
+  email_id = input("Enter email id: ")
 
-email_id = input("Enter email id: ")
+  username = email_id[:email_id.index('@')]
 
-username = email_id[:email_id.index('@')]
+  domain = email_id[email_id.index('@')+1:]
 
-domain = email_id[email_id.index('@')+1:]
-
-a=domain.upper()
-print(f"username: {username} and domain: {(a)}")
+  a=domain.upper()
+  print(f"username: {username} and domain: {(a)}")
 
 
 
